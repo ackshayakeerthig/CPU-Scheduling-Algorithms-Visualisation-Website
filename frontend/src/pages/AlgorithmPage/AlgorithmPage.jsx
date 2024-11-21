aacee66d141dfd8cf7aef974df26c2e11b23d92f
SRNI-2005 <vssreenivaas2005@gmail.com>
2024-11-22 01:58:46 +0530

    Initialise algorithm files in algorithms folder

--- frontend/src/pages/AlgorithmPage/AlgorithmPage.jsx
+++ frontend/src/pages/AlgorithmPage/AlgorithmPage.jsx
@@ -6,6 +6,10 @@ import OutputCard from "../../components/OutputCard/OutputCard";
 
 const algorithms = {
   SRTF: React.lazy(() => import("../../algorithms/SRTF.js")),
+	FCFS: React.lazy(() => import("../../algorithms/FCFS.js")),
+	RoundRobin: React.lazy(() => import("../../algorithms/RR.js")),
+	Priority: React.lazy(() => import("../../algorithms/priority.js")),
+	SJF: React.lazy(() => import("../../algorithms/SJF.js")),
 };
 
 function selectAlgorithm(algorithmName) {
